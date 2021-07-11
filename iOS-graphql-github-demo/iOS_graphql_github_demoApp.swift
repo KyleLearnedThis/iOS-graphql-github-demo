739c397e5cb5b04870582a84daa86ae52a5dfd03
Kyle Learned <kyle.learned.this@gmail.com>
2021-07-11 02:36:01 -0700

    Initial Commit

---
+++ iOS-graphql-github-demo/iOS_graphql_github_demoApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_graphql_github_demoApp.swift
+//  iOS-graphql-github-demo
+//
+//  Created by KyleLearnedThis on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct iOS_graphql_github_demoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
